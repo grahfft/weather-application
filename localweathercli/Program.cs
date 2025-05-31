90b78e1290843193b494ca52c8646ae165597d3a
Dennis Lassell <dennis.lassell@gmail.com>
2025-05-31 10:15:53 -0400

    Command Line now makes http request to local host

--- localweathercli/Program.cs
+++ localweathercli/Program.cs
@@ -1,38 +1,53 @@
 ﻿
 using DotMake.CommandLine;
-Cli.Run<RootWithNestedChildrenCliCommand>(args);
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Web;
 
+await Cli.RunAsync<RootWithNestedChildrenCliCommand>(args);
 
 [CliCommand(Description = "A root cli command with nested children")]
 public class RootWithNestedChildrenCliCommand
 {
-
     [CliArgument(Description = "subcommand you want to run")]
     public string command { get; set; } = "get-current-weather";
 
-    public void Run(CliContext context)
+    public async Task RunAsync(CliContext context)
     {
-        if (context.IsEmptyCommand())
-            context.ShowHierarchy();
-        else
-            context.ShowValues();
+        context.ShowHelp();
     }
 
     [CliCommand(Description = "Gets the Current Weather for a given Zipcode")]
     public class GetCurrentWeatherSubCliCommand
     {
-        [CliOption(Description = "Description for Option1")]
-        public string Option1 { get; set; } = "DefaultForOption1";
-
         [CliArgument(Description = "Where you want to get the weather")]
-        public string zipcode { get; set; }
+        public string Zipcode { get; set; }
 
-         [CliArgument(Description = "Unit of Measure of the temperature ")]
-        public string unit { get; set; }
+        [CliArgument(Description = "Unit of Measure of the temperature ")]
+        public string Unit { get; set; }
 
-        public void Run(CliContext context)
+        private HttpClient client = new();
+
+        public async Task RunAsync(CliContext context)
         {
-            
+            if (context.IsEmptyCommand())
+            {
+                context.ShowHelp();
+            }
+            else
+            {
+                Console.WriteLine("Getting Current weather");
+                var uriBuilder = new UriBuilder($"http://localhost:5183/Weather/Current/{this.Zipcode}");
+                var parameters = HttpUtility.ParseQueryString(string.Empty);
+                parameters["unit"] = this.Unit;
+                uriBuilder.Query = parameters.ToString();
+
+                Uri uri = uriBuilder.Uri;
+                var response = await this.client.GetAsync(uri);
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"{JsonObject.Parse(jsonResponse)}\n");
+            }
         }
     }
 }
