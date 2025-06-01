bbc30a1bf7a55d054aa4c495baf3fbbb1a0edfb2
Dennis Lassell <dennis.lassell@gmail.com>
2025-05-31 21:51:00 -0400

    Send http request to OpenWeather Api; Parses response and serializes it into internal object

--- WeatherApplication/repositories/OpenWeatherRepository.cs
+++ WeatherApplication/repositories/OpenWeatherRepository.cs
@@ -1,26 +1,34 @@
 using System.Web;
+using Newtonsoft.Json;
 
 public class OpenWeatherRepository : IWeatherRepository
 {
     private HttpClient client = new();
     public async Task<WeatherForecast> getCurrentForecast(string zipcode, string unit)
     {
-        // var uriBuilder = new UriBuilder($"http://localhost:5183/Weather/Current/{this.Zipcode}");
-        // var parameters = HttpUtility.ParseQueryString(string.Empty);
-        // parameters["unit"] = "";
-        // uriBuilder.Query = parameters.ToString();
+        // TODO Create enum for easy mapping of Fahrenheit -> Imperial and back for unit mapping
+        var uriBuilder = new UriBuilder($"https://api.openweathermap.org/data/2.5/weather");
+        var parameters = HttpUtility.ParseQueryString(string.Empty);
+        parameters["zip"] = zipcode.Substring(0, 5);
+        parameters["appid"] = "4783376f20ba8dec300467cb4d4cb209";
+        parameters["units"] = "imperial";
+        uriBuilder.Query = parameters.ToString();
 
-        // Uri uri = uriBuilder.Uri;
-        // var response = await this.client.GetAsync(uri);
+        Uri uri = uriBuilder.Uri;
+        var response = await this.client.GetAsync(uri);
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        // Console.WriteLine($"{jsonResponse}");
 
-        // var jsonResponse = await response.Content.ReadAsStringAsync();
+        var openWeatherForecast = System.Text.Json.JsonSerializer.Deserialize<OpenWeatherForecast>(jsonResponse);
+        Console.WriteLine($"{JsonConvert.SerializeObject(openWeatherForecast)}");
+
         return new WeatherForecast
             (
-                60,
+                (int)openWeatherForecast.main.temp,
                 unit,
-                45.67,
-                54.36,
-                true
+                openWeatherForecast.coord.lat,
+                openWeatherForecast.coord.lon,
+                openWeatherForecast.weather.Exists(report => report.main == "Rain" || report.main == "Drizzle")
             );
     }
 }
