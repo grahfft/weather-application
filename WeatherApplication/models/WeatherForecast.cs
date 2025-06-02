1f2a8c3337cbb6075e5ceb36b09484a901bd2ade
Dennis Lassell <dennis.lassell@gmail.com>
2025-06-02 12:07:39 -0400

    Add in day count validation; move RainPossible to sub classes for better verbage

--- WeatherApplication/models/WeatherForecast.cs
+++ WeatherApplication/models/WeatherForecast.cs
@@ -1,11 +1,10 @@
 public abstract class WeatherForecast
 {
-    public WeatherForecast(string unit, double lat, double lon, Boolean rainPossibleToday)
+    public WeatherForecast(string unit, double lat, double lon)
     {
         this.Unit = unit;
         this.Lat = lat;
         this.Lon = lon;
-        this.RainPossibleToday = rainPossibleToday;
     }
 
     
@@ -13,26 +12,29 @@ public abstract class WeatherForecast
 
     public double Lat { get; }
     public double Lon { get; }
-
-    public Boolean RainPossibleToday { get; }
 }
 
 public class CurrentForecast : WeatherForecast
 {
-    public CurrentForecast(int currentTemperature, string unit, double lat, double lon, bool rainPossibleToday) : base(unit, lat, lon, rainPossibleToday)
+    public CurrentForecast(int currentTemperature, string unit, double lat, double lon, bool rainPossibleToday) : base(unit, lat, lon)
     {
         this.CurrentTemperature = currentTemperature;
     }
 
     public int CurrentTemperature { get; }
+    public bool RainPossibleToday { get; }
 }
 
 public class AverageForecast : WeatherForecast
 {
-    public AverageForecast(int averageTemperature, string unit, double lat, double lon, bool rainPossibleToday) : base(unit, lat, lon, rainPossibleToday)
+    public AverageForecast(int averageTemperature, string unit, double lat, double lon, bool rainPossibleInPeriod) : base(unit, lat, lon)
     {
         this.AverageTemperature = averageTemperature;
+        this.RainPossibleInPeriod = rainPossibleInPeriod;
     }
 
     public int AverageTemperature { get; }
+
+    public bool RainPossibleInPeriod { get; }
+
 }
